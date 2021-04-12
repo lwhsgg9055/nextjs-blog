a6f2c0ef310cc10570cf40115fcc9deee611feca
lwhsgg9055 <82318889+lwhsgg9055@users.noreply.github.com>
2021-04-12 18:06:07 +0900

    Delete Users/In line/nextjs-blog directory

--- Users/In line/nextjs-blog/pages/posts/first-post.js
+++
@@ -1,8 +0,0 @@
-export default function FirstPost() {
-
-
-
-
-
-  return <h1>　good morning,my dear professors.my name is song 류문호 ,i am 20 years old .i come from 류주시 ,a very beautiful aicent city. 감사합니다 ^^! .</h1>
-}
